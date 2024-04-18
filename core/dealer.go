62f223314fe7821cfa0eebd7c53545e152b65876
Marcos Travasso <marcos.travasso42@gmail.com>
2024-04-17 23:04:17 -0300

    add dealer interface

---
+++ core/dealer.go
@@ -0,0 +1,9 @@
+package core
+
+import "xp-task-dealer/core/models"
+
+type Dealer interface {
+	GetDeveloperForTask(task models.Task, developers []models.Developer) (models.Developer, error)
+	GetTaskForDeveloper(developer models.Developer, tasks []models.Task) (models.Task, error)
+	GetPairForDeveloper(developer models.Developer, task models.Task, developers []models.Developer) ([]models.Developer, error)
+}
